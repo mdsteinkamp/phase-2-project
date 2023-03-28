86f1fff42af12af1453e1b8ed7e82b9d151b4bc5
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-03-27 20:49:09 -0700

    installed react switch & added switch component to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactSwitch from "react-switch";
 import { Link } from "react-router-dom";
 
 export default function NavBar() {
@@ -7,6 +8,7 @@ export default function NavBar() {
       <span><Link to="/">Home</Link></span>
       <span><Link to="/tracks">My Tracks</Link></span>
       <span><Link to="/addtrack">Add a Track</Link></span>
+      <span>Light Mode<ReactSwitch /></span>
     </nav>
   )
 }
