3328c885a696e5864260b61465243956f00ffc38
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-03-27 21:55:55 -0700

    initial styling for TrackCards in styled.js file

--- src/components/TrackCard.js
+++ src/components/TrackCard.js
@@ -7,8 +7,12 @@ export default function TrackCard({name, artist, img, mode, difficulty, complete
       <p>by {artist}</p>
       <img src={img} alt={name} />
       <p>key: {mode}</p>
-      <p>difficulty {difficulty}</p>
-      <p>completed </p>
+      <div className="bottom">
+        <span>difficulty {difficulty}</span>
+        <form>Completed
+          <input type="checkbox" id="completed"></input>
+        </form>
+      </div>
     </TrackCardContainer>
   )
 }
--- src/components/styles/TrackCard.Container.styled.js
+++ src/components/styles/TrackCard.Container.styled.js
@@ -1,6 +1,18 @@
 import styled from "styled-components";
 
 export const TrackCardContainer = styled.div`
+  border-style: solid;
+  border-color: black;
+  background-color: aquamarine;
+
+  p {
+    text-align: center;
+  }
+
+  .bottom {
+    display: flex;
+    justify-content: space-between;
+  }
 
 `
 
